4c352cb0119b8f6bb22de9670f07b107816a3af9
G. KIBRIA JANI <30300651+janishikder@users.noreply.github.com>
2021-03-24 23:13:38 +0600

    Unessential console log removed.

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-console.log("Client side javascirpt running");
 const weatherForm = document.querySelector("form");
 const searchElement = document.querySelector("input");
 const msg1 = document.querySelector("#message-1");
