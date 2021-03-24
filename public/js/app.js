8066fe862999fa52d5329435ef5cb182fa8aa340
G. KIBRIA JANI <30300651+janishikder@users.noreply.github.com>
2021-03-24 23:08:56 +0600

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+
+const weatherForm = document.querySelector("form");
+const searchElement = document.querySelector("input");
+const msg1 = document.querySelector("#message-1");
+const msg2 = document.querySelector("#message-2");
+
+weatherForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const location = searchElement.value;
+    msg1.textContent = "Loading...";
+    msg2.textContent = "";
+
+    fetch("http://localhost:3000/weather?address="+ encodeURIComponent(location)).then((response)=>{
+   response.json().then((data)=>{
+       if(data.error){
+           msg1.textContent  = data.error;
+       }else{
+           msg1.textContent = data.location;
+           msg2.textContent  = data.forecast;
+           
+       }
+   })
+})
+})
